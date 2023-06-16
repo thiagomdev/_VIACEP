67dddf7c121d729fc2e869c1706f66b88a52bbd1
Thiago <Thiago Monteiro>
2023-06-16 18:57:40 -0300

    Exchanging protocol Decodable to codable

--- VIACEP/Features/Core/WebService/Networking.swift
+++ VIACEP/Features/Core/WebService/Networking.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 protocol NetworkingProtocol {
-    func execute<T: Decodable>(request: Request, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> RequestTask
+    func execute<T: Codable>(request: Request, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> RequestTask
 }
 
 final class Networking: NetworkingProtocol {
-    func execute<T>(request: Request, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> RequestTask where T : Decodable {
+    func execute<T>(request: Request, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> RequestTask where T : Codable {
         Tasking<T>(request: request, completion: completion)
     }
 }
